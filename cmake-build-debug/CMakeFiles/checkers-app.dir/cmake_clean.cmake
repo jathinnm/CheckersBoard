7e0849a3e4c0ba9141ba63cfd0e145ff57720a7a
Jathin Nama <47833121+jathinnm@users.noreply.github.com>
2021-05-06 17:44:13 -0500

    Fix spacing and add comments

---
+++ cmake-build-debug/CMakeFiles/checkers-app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checkers-app.dir/apps/checkers_app_main.cc.o"
+  "checkers-app"
+  "checkers-app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/checkers-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
